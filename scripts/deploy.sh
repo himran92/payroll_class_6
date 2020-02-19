7dd5d7a53691d9585b9d6146f829a8fbb0cbdd93
himran1992 <himran@contractor.chanzuckerberg.com>
2020-02-19 13:48:34 -0500

    change

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
-scp /home/travis/build/himran92/payroll_class_6/target/payroll-${TRAVIS_BUILD_NUMBER}.jar ec2-user@ec2-3-20-204-183.us-east-2.compute.amazonaws.com:~/
-rm -rf *
+rm payroll-0.0.1-SNAPSHOT
+scp /home/travis/build/himran92/payroll_class_6/target/payroll-0.0.1-SNAPSHOT.jar ec2-user@ec2-3-20-204-183.us-east-2.compute.amazonaws.com:~/
 killall -9 java
-java -jar payroll-${TRAVIS_BUILD_NUMBER}.jar
+java -jar payroll-0.0.1-SNAPSHOT.jar
