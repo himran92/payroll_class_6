166daff2395c34b952b4f7f999bb2d06fbbf416c
himran1992 <himran@contractor.chanzuckerberg.com>
2020-02-19 14:35:38 -0500

    change

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,2 @@
 scp /home/travis/build/himran92/payroll_class_6/target/payroll-0.0.1-SNAPSHOT.jar ec2-user@ec2-3-20-204-183.us-east-2.compute.amazonaws.com:~/
-ssh "ls -al ~/"
-killall -9 java
-java -jar ~/payroll-0.0.1-SNAPSHOT.jar
+ssh ec2-user@ec2-3-20-204-183.us-east-2.compute.amazonaws.com "ls -al ~/"
